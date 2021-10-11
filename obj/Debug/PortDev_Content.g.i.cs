1a64a04e20953d69990efaea82bb7f3aae004f4f
Wietse de Jonge <73637701+wietsedejonge@users.noreply.github.com>
2021-10-12 00:28:47 +0200

    Adds a navigation bar, with mouse hover functions

--- obj/Debug/PortDev_Content.g.i.cs
+++ obj/Debug/PortDev_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons8-menu-24(1).png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons8-delete-24.png")]
 
 
